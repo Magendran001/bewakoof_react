fb927c1e5dd49f4457dcf35b460cc282c4256035
V A Magendran <95860580+Magendran001@users.noreply.github.com>
2022-06-18 00:04:07 +0530

    drawer chakra ui

---
+++ src/components/drawer/drawer.jsx
@@ -0,0 +1,36 @@
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+  } from '@chakra-ui/react'
+import { useState } from "react"
+function Drawerleft() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [placement, setPlacement] = useState('right')
+  
+    return (
+      <>
+        
+        <Button colorScheme='blue' onClick={onOpen}>
+          Open
+        </Button>
+        <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
+            <DrawerBody>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </>
+    )
+  }
+
+  export default Drawerleft
